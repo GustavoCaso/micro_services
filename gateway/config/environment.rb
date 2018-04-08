1c89f6384092d8c979706d74f3a9716d00866b9a
Gustavo Caso <gustavocaso@gmail.com>
2018-04-08 12:49:34 +0200

    gateway: Introduce better folder structure

---
+++ gateway/config/environment.rb
@@ -0,0 +1,24 @@
+# Require gems we care about
+require 'rubygems'
+require "bundler/setup"
+
+require 'sinatra'
+require 'yaml'
+require 'json'
+
+# Some helper constants for path-centric logic
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+
+APP_NAME = APP_ROOT.basename.to_s
+
+# load lib folder
+Dir[APP_ROOT.join('lib', '**/*.rb')].each { |file| require file }
+
+
+# Let's create the endpoints provided by the yml file
+route_builder = RouteBuilder.new(self)
+
+YAML.load_file('config.yaml')['urls'].each do |url|
+  route_builder.call(url)
+end
+
