3c9e2e057c029e44c7c82df269772d81573c6def
Gustavo Caso <gustavocaso@gmail.com>
2018-04-09 23:28:12 +0200

    driver-location: Improve logic for time_frame_calculator

--- driver-location/lib/services/time_frame_calculator.rb
+++ driver-location/lib/services/time_frame_calculator.rb
@@ -1,8 +1,48 @@
 module Services
   class TimeFrameCalculator
-    def self.call
+    VALID_KEYS = %w[minutes hours days]
+    MINUTES_IN_SECONDS = 60
+    HOURS_IN_SECONDS = 60 * 60
+    DAYS_IN_SECONDS = 24 * 60 * 60
+
+    def call(params = {})
+      return default_time_frame unless params.keys.any? { |key| VALID_KEYS.include?(key) }
+
+      total_to_rest = calculate_total_time(params)
+
+      return_epoch_times(total_to_rest)
+    end
+
+    private
+
+    def calculate_total_time(params)
+      total = 0
+      params.each do |key, value|
+        total += send(key.to_sym, value.to_i)
+      end
+      total
+    end
+
+    def minutes(value)
+      (value * MINUTES_IN_SECONDS)
+    end
+
+    def hours(value)
+      (value * HOURS_IN_SECONDS)
+    end
+
+    def days(value)
+      (value * DAYS_IN_SECONDS)
+    end
+
+    # Default time range of 5 minutes
+    def default_time_frame
+      return_epoch_times(minutes(5))
+    end
+
+    def return_epoch_times(value)
       now = Time.now
-      check_time = now - ( 5 * 60 )
+      check_time = now - value
       [check_time.to_i, now.to_i]
     end
   end
--- driver-location/spec/lib/time_frame_calculator_spec.rb
+++ driver-location/spec/lib/time_frame_calculator_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../lib/services/time_frame_calculator'
 
 RSpec.describe Services::TimeFrameCalculator do
-  subject { described_class.call }
+  subject { described_class.new }
 
   before do
     # The time in Epoch for 2018 is 1514761200
@@ -9,9 +9,33 @@ RSpec.describe Services::TimeFrameCalculator do
     expect(Time).to receive(:now).and_return(Time.new(2018))
   end
 
-  it 'returns the min and max' do
-    min, max = subject
-    expect(min).to eq 1514760900
-    expect(max).to eq 1514761200
+  context 'when no valid keys are pass' do
+    let(:params) { {} }
+
+    it 'returns the min and max' do
+      min, max = subject.call(params)
+      expect(min).to eq 1514760900
+      expect(max).to eq 1514761200
+    end
+  end
+
+  context 'when valid params are passed' do
+    let(:params) { {'minutes' => 10 } }
+
+    it 'returns the min and max' do
+      min, max = subject.call(params)
+      expect(min).to eq 1514760600 # 1514761200 - 600
+      expect(max).to eq 1514761200
+    end
+  end
+
+  context 'accumulate value when passed multiple params' do
+    let(:params) { {'minutes' => 10, 'hours' => 1 } }
+
+    it 'returns the min and max' do
+      min, max = subject.call(params)
+      expect(min).to eq 1514757000 # 1514761200 - 4200
+      expect(max).to eq 1514761200
+    end
   end
 end
