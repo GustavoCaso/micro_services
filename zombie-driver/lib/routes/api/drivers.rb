e5f3e42e0ce9003548195b1632589eb6b429d32d
Gustavo Caso <gustavocaso@gmail.com>
2018-04-09 15:42:45 +0200

    zombie-driver: Add specs for drivers endpoint

--- zombie-driver/lib/routes/api/drivers.rb
+++ zombie-driver/lib/routes/api/drivers.rb
@@ -2,12 +2,9 @@ get '/drivers/:id' do
   content_type :json
 
   driver_id = params[:id]
-  response = Services::HttpRequest.get("http://localhost:4002/drivers/#{driver_id}/locations")
-  body = response.body
-  result = JSON.parse(body)
-  first_coordinates = [result[0]['longitude'].to_f,  result[0]['latitude'].to_f]
-  last_coordinates = [result[-1]['longitude'].to_f,  result[-1]['latitude'].to_f]
-  distance = Services::DistanceCalculator.call(first_coordinates, last_coordinates)
-  zombie = distance < 500
-  { id: params[:id], zombie: zombie }.to_json
+  distance_service = Services::Distance.new
+  result = distance_service.call(driver_id)
+  return {}.to_json if result.failure?
+  zombie = result.value < 500
+  { id: driver_id, zombie: zombie }.to_json
 end
---
+++ zombie-driver/spec/integration/drivers_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_integration'
+
+RSpec.describe 'Drivers endpoint' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  let(:failure_result) { double('Result::Failure', success?: false, failure?: true, value: 'Error') }
+  let(:success_result_zombie) { double('Result::Success', success?: true, failure?: false, value: 498) }
+  let(:success_result_not_zombie) { double('Result::Success', success?: true, failure?: false, value: 1000.7877) }
+
+  context 'when everything works as expected' do
+    it "triggers Services::Distance" do
+      expect(Services::Distance).to receive_message_chain(:new, :call).with('678').and_return(success_result_zombie)
+      get '/drivers/678'
+    end
+
+    it "returns correct response" do
+      expect(Services::Distance).to receive_message_chain(:new, :call).with('678').and_return(success_result_zombie)
+      get '/drivers/678'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq('{"id":"678","zombie":true}')
+    end
+
+    it "returns correct response when is not zombie" do
+      expect(Services::Distance).to receive_message_chain(:new, :call).with('678').and_return(success_result_not_zombie)
+      get '/drivers/678'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq('{"id":"678","zombie":false}')
+    end
+  end
+
+  context 'when something fails' do
+    it "triggers Services::Distance" do
+      expect(Services::Distance).to receive_message_chain(:new, :call).with('678').and_return(failure_result)
+      get '/drivers/678'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq('{}')
+    end
+  end
+end
