9c3760aec953ac36110a85db2e1d97013aa924ab
Gustavo Caso <gustavocaso@gmail.com>
2018-04-09 07:38:58 +0200

    dricer-location: Use servceis in locations endpoint

--- driver-location/lib/routes/api/drivers.rb
+++ driver-location/lib/routes/api/drivers.rb
@@ -1,8 +1,7 @@
 get '/drivers/:id/locations' do
   content_type :json
-  key = "drivers:coordinates:#{params[:id]}"
-  now = Time.now
-  check_time = now - ( 5 * 60 )
-  result = REDIS.zrangebyscore(key, check_time.to_i, now.to_i)
-  result.map { |r| JSON.parse(r) }.to_json
+
+  service = Services::Redis::CoordinatesQuery.new
+  locations = service.call(params[:id])
+  locations.to_json
 end
