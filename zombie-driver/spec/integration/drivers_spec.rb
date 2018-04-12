b97a3d1180890fd3f424181dd06d04d7ad2e5efe
Gustavo Caso <gustavocaso@gmail.com>
2018-04-12 23:56:45 +0200

    zombie-driver: run real integration specs on drivers endpoint

--- zombie-driver/spec/integration/drivers_spec.rb
+++ zombie-driver/spec/integration/drivers_spec.rb
@@ -7,34 +7,54 @@ RSpec.describe 'Drivers endpoint' do
     Sinatra::Application
   end
 
-  let(:failure_result) { double('Result::Failure', success?: false, failure?: true, value: 'Error') }
-  let(:success_result_zombie) { double('Result::Success', success?: true, failure?: false, value: 498) }
-  let(:success_result_not_zombie) { double('Result::Success', success?: true, failure?: false, value: 1000.7877) }
-
-  context 'when everything works as expected' do
-    it "triggers Services::Distance" do
-      expect(Services::Distance).to receive_message_chain(:new, :call).with('678').and_return(success_result_zombie)
-      get '/drivers/678'
+  context 'when distance is bigger than 500 meters' do
+    before do
+      coordinates = '[{"latitude":40.4165,"longitude":-3.7026},{"latitude":48.8534,"longitude":-2.3488}]'
+      result = Result::Success.new(coordinates)
+      expect_any_instance_of(Services::HttpRequest).to receive(:get).and_return(result)
     end
 
     it "returns correct response" do
-      expect(Services::Distance).to receive_message_chain(:new, :call).with('678').and_return(success_result_zombie)
-      get '/drivers/678'
-      expect(last_response).to be_ok
-      expect(last_response.body).to eq('{"id":"678","zombie":true}')
-    end
-
-    it "returns correct response when is not zombie" do
-      expect(Services::Distance).to receive_message_chain(:new, :call).with('678').and_return(success_result_not_zombie)
       get '/drivers/678'
       expect(last_response).to be_ok
       expect(last_response.body).to eq('{"id":"678","zombie":false}')
     end
   end
 
-  context 'when something fails' do
-    it "triggers Services::Distance" do
-      expect(Services::Distance).to receive_message_chain(:new, :call).with('678').and_return(failure_result)
+  context 'when distance is smaller than 500 meters' do
+    before do
+      coordinates = '[{"latitude":40.6826596,"longitude":-3.6151842},{"latitude":40.6825105,"longitude":-3.6170551}]'
+      result = Result::Success.new(coordinates)
+      expect_any_instance_of(Services::HttpRequest).to receive(:get).and_return(result)
+    end
+
+    it 'returns correct response' do
+      get '/drivers/678'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq('{"id":"678","zombie":true}')
+    end
+  end
+
+  context 'when request fails' do
+    before do
+      result = Result::Failure.new('Fail request')
+      expect_any_instance_of(Services::HttpRequest).to receive(:get).and_return(result)
+    end
+
+    it 'returns correct response' do
+      get '/drivers/678'
+      expect(last_response).to_not be_ok
+      expect(last_response.body).to eq('{}')
+    end
+  end
+
+  context 'when fails at parsing the response' do
+    before do
+      result = Result::Success.new('aaa')
+      expect_any_instance_of(Services::HttpRequest).to receive(:get).and_return(result)
+    end
+
+    it 'returns correct response' do
       get '/drivers/678'
       expect(last_response).to_not be_ok
       expect(last_response.body).to eq('{}')
