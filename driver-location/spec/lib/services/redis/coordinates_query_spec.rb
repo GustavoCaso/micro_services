7ef3f9ec8b22158e716d4e13fad326fcecef3869
Gustavo Caso <gustavocaso@gmail.com>
2018-04-12 18:33:15 +0200

    driver-location: test redis service using real redis services

--- driver-location/spec/lib/services/redis/coordinates_query_spec.rb
+++ driver-location/spec/lib/services/redis/coordinates_query_spec.rb
@@ -2,14 +2,12 @@ require_relative '../../../../lib/services/redis/keys'
 require_relative '../../../../lib/services/redis/coordinates_query'
 
 RSpec.describe Services::Redis::CoordinatesQuery do
-  let(:mock_redis) { double('REDIS').as_null_object }
   let(:mock_time_frame) { instance_double('TimeFrameCalculator', call: [1,2]) }
-  subject { described_class.new(redis: mock_redis, time_frame_calculator: mock_time_frame ) }
+  let(:id) { 'testing:coordinates:query:1' }
+  let(:params) { { 'id' => id, 'minutes' => 5 } }
+  subject { described_class.new(time_frame_calculator: mock_time_frame ) }
 
   context 'when pass an id and time arguments' do
-    let(:id) { 1 }
-    let(:params) { { 'id' => id, 'minutes' => 5 } }
-
     it 'gets the valid key' do
       expect(Services::Redis::Keys::COORDINATES).to receive(:call).with(id)
       subject.call(params)
@@ -19,19 +17,29 @@ RSpec.describe Services::Redis::CoordinatesQuery do
       expect(mock_time_frame).to receive(:call).with('minutes' => 5)
       subject.call(params)
     end
+  end
 
-    it 'executes query against REDIS' do
-      expect(mock_redis).to receive(:zrangebyscore).with('drivers:coordinates:1', 1, 2)
-      subject.call(params)
+  context 'when there is data in redis' do
+
+    before do
+      key = Services::Redis::Keys::COORDINATES.call('testing:coordinates:query:1')
+      coordinates_1 = { longitude: 1, latitude: 2 }
+      coordinates_2 = { longitude: 2, latitude: 3 }
+      REDIS.zadd(key, 1, coordinates_1.to_json)
+      REDIS.zadd(key, 2, coordinates_2.to_json)
     end
 
-    it 'parses all results from REDIS' do
+    after do
+      key = Services::Redis::Keys::COORDINATES.call('testing:coordinates:query:1')
+      REDIS.zremrangebyscore(key, 1, 2)
+    end
+
+    it 'executes query against REDIS' do
       redis_response = [
-        "{\"longitude\":\"1\",\"latitude\":\"2\",\"updated_at\":\"2018-04-08 17:57:42 +0200\"}",
-        "{\"longitude\":\"1\",\"latitude\":\"2\",\"updated_at\":\"2018-04-08 17:57:43 +0200\"}"
+        "{\"longitude\": 1,\"latitude\": 2 }",
+        "{\"longitude\": 2,\"latitude\": 3 }"
       ]
       expected_result = redis_response.map { |r| JSON.parse(r) }
-      allow(mock_redis).to receive(:zrangebyscore).and_return(redis_response)
       result = subject.call(params)
       expect(result).to be_a Result::Success
       expect(result.value).to eq(expected_result)
@@ -57,7 +65,7 @@ RSpec.describe Services::Redis::CoordinatesQuery do
     end
 
     it 'does not executes query' do
-      expect(mock_redis).to_not receive(:zrangebyscore)
+      expect_any_instance_of(Redis).to_not receive(:zrangebyscore)
       subject.call(params)
     end
   end
